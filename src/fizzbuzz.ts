0a16c64869a4e1e2bd33ab5f829107d382dd9e48
Lluis Alsina Navarro <lalsina883@gmail.com>
2025-05-20 15:49:16 +0200

    Refactorizacion de la function fizzbuzz.

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,8 +1,15 @@
-export const fizzbuzz = (number): number | "fizz" | "buzz" | "fizzbuzz" => {
+export const fizzbuzz = (number): string | number => {
     if (typeof number !== 'number') throw new Error('parameter provided must be a number ')
     if (Number.isNaN(number)) throw new Error('parameter provided must be a number')
-    if (number % 5 === 0 && number % 3 === 0) return 'fizzbuzz'
-    if (number % 3 === 0) return 'fizz'
-    if (number % 5 === 0) return 'buzz'
-    return number
+
+    const multiplies = { 3: 'fizz', 5: 'buzz'}        
+    let output = '' ;
+
+    Object
+    .entries(multiplies)
+    .forEach(([multiplier,word]) : void => {
+        if ( number % multiplier === 0) output += word ;
+    })
+
+    return output === '' ? number : output 
 }
